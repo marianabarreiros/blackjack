ab31d5f66f288a6306ac854595566a52d3b7531f
Mariana Barreiros <marinabarreiros.ti@gmail.com>
2023-10-19 01:53:22 -0300

    Criar método para embaralhar cartas.

--- src/main/java/org/blackjack/Baralho.java
+++ src/main/java/org/blackjack/Baralho.java
@@ -2,20 +2,24 @@ package org.blackjack;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 @Getter
 @EqualsAndHashCode
 public class Baralho {
-    private ArrayList<Carta> cartas;
+    private final ArrayList<Carta> cartas;
 
-    public Baralho(){
+    public Baralho() {
         this.cartas = this.construirBaralho();
     }
 
-    public ArrayList<Carta> construirBaralho(){
-       return Carta.obterCartas();
+    public ArrayList<Carta> construirBaralho() {
+        return Carta.obterCartas();
+    }
+
+    public void embaralharCartas() {
+        Collections.shuffle(cartas);
     }
 }
--- src/main/java/org/blackjack/Blackjack.java
+++ src/main/java/org/blackjack/Blackjack.java
@@ -3,11 +3,9 @@ package org.blackjack;
 public class Blackjack {
 
     public static void main(String[] args) {
-
-
-    }
-
-    public static void iniciar(){
-
+        Baralho baralho = new Baralho();
+        System.out.println(baralho.construirBaralho());
+        baralho.embaralharCartas();
+        System.out.println(baralho.getCartas());
     }
 }
