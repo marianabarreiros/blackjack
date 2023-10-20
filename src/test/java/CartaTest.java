25915e1eda4c55b6a79ffa043c799f825a15c8d8
Mariana Barreiros <marinabarreiros.ti@gmail.com>
2023-10-20 12:05:30 -0300

    Criar testes unitários para classe carta.

---
+++ src/test/java/CartaTest.java
@@ -0,0 +1,55 @@
+import org.blackjack.Carta;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CartaTest {
+
+    Carta carta;
+
+    @BeforeEach
+    public void setUp() {
+        carta = new Carta("C", "A");
+    }
+
+    @Test
+    @DisplayName("Construtor deve inicialzar objeto carta corretamente.")
+    public void testConstrutorCarta() {
+        assertNotNull(carta);
+        assertEquals("C", carta.getTipo());
+        assertEquals(11, carta.getValor());
+    }
+
+    @Test
+    @DisplayName("Método toString deve formatar carta corretamente.")
+    public void testToString() {
+        assertEquals("A-C", carta.toString());
+    }
+
+    @Test
+    @DisplayName("Método getValor deve retornar valores corretamente.")
+    public void testGetValor() {
+        assertEquals(11, carta.getValor());
+
+        Carta cartaNumerica = new Carta("D", "7");
+        assertEquals(7, cartaNumerica.getValor());
+
+        Carta cartaFigura = new Carta("H", "K");
+        assertEquals(10, cartaFigura.getValor());
+    }
+
+    @Test
+    @DisplayName("Carta deve ser um As")
+    public void testIsAce() {
+        assertTrue(carta.isAce());
+    }
+
+    @Test
+    @DisplayName("Carta diferente de A não é um As")
+    public void testNoIsAce() {
+        Carta outraCarta = new Carta("S", "9");
+        assertFalse(outraCarta.isAce());
+    }
+}
